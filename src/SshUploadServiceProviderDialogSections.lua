25cac7713cefbbef55fa3bb7b59f1b417288edde
Knut Vidar Siem <knutsiem@gmail.com>
2012-12-15 23:58:06 +0100

    Compacted export/publish dialog SSH section layout

--- src/SshUploadServiceProviderDialogSections.lua
+++ src/SshUploadServiceProviderDialogSections.lua
@@ -9,29 +9,46 @@ function SshUploadServiceProviderDialogSections.topSections (viewFactory, proper
 			synopsis = function (props)
 				return props["user"] .. "@" .. props["host"] .. ":" .. props["destination_path"]
 			end,
-			viewFactory:static_text {
-					title = "User",
+			viewFactory:row {
+				spacing = viewFactory:control_spacing(),
+				viewFactory:column {
+					viewFactory:static_text {
+						title = "User",
+					},
+					viewFactory:edit_field {
+						value = LrView.bind "user",
+						width_in_chars = 10
+					}
+				},
+				viewFactory:column {
+					viewFactory:static_text {
+						title = "Target host",
+					},
+					viewFactory:edit_field {
+						value = LrView.bind "host",
+						width_in_chars = 10
+					}
+				},
+				viewFactory:column {
+					viewFactory:static_text {
+						title = "Destination path",
+					},
+					viewFactory:edit_field {
+						value = LrView.bind "destination_path",
+						fill = 1
+					},
+					fill = 1
+				}
 			},
-			viewFactory:edit_field {
-				value = LrView.bind "user"
-			},
-			viewFactory:static_text {
-					title = "Target host",
-			},
-			viewFactory:edit_field {
-				value = LrView.bind "host"
-			},
-			viewFactory:static_text {
-					title = "Identity file",
-			},
-			viewFactory:edit_field {
-				value = LrView.bind "identity"
-			},
-			viewFactory:static_text {
-					title = "Destination path",
-			},
-			viewFactory:edit_field {
-				value = LrView.bind "destination_path"
+			viewFactory:spacer {},
+			viewFactory:row {
+				viewFactory:static_text {
+						title = "Identity file",
+				},
+				viewFactory:edit_field {
+					value = LrView.bind "identity",
+					fill = 1
+				}
 			}
 		}
 	}
